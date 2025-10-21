992cd10ede5497151a2566d8e27e5f0f5c81ffca
Oskar Rowicki <oskar.rowicki@gmail.com>
2025-10-21 10:17:48 +0200

    Add using collection_t

--- named_poset_collections.cpp
+++ named_poset_collections.cpp
@@ -1,12 +1,13 @@
-/* 
+/*
  * notes:
- * 
+ *
  * for collections most likely use:
  * using collection_t =
- * unordered_map<long, map<string, pair<array<bitset<N>, N>, array<bitset<N>, N>>>>
+ * unordered_map<long, map<string, pair<array<bitset<N>, N>, array<bitset<N>,
+ * N>>>>
  *
  *
- * figure out static global init stuff to avoid init order problem 
+ * figure out static global init stuff to avoid init order problem
  * (program has to work when linked with .o files swapped around)
  *
  * namespace cxx
@@ -14,8 +15,22 @@
  * dont hold user's `char const *` -> convert them to cpp strings, to prevent
  * user's access to the collection's contents.
  *
-*/
-
+ */
 
 #include "named_poset_collections.h"
+#include <bitset>
+#include <map>
+#include <string>
+#include <unordered_map>
 
+using std::array;
+using std::bitset;
+using std::map, std::unordered_map;
+using std::pair;
+using std::string;
+
+constexpr int N = 32;
+
+using collection_t =
+    unordered_map<long,
+                  map<string, pair<array<bitset<N>, N>, array<bitset<N>, N>>>>;
