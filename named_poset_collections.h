70502de329e36034c447e4cad5d787ec051aa8cb
Oskar Rowicki <oskar.rowicki@gmail.com>
2025-10-22 16:31:56 +0200

    Add namespace cxx to the header file

--- named_poset_collections.h
+++ named_poset_collections.h
@@ -4,7 +4,8 @@
 #include <stddef.h>
 
 #ifdef __cplusplus
-  extern "C" {
+namespace cxx {
+extern "C" {
 #endif
 
 long npc_new_collection(void);
@@ -14,8 +15,8 @@ bool npc_new_poset(long id, char const *name);
 void npc_delete_poset(long id, char const *name);
 bool npc_copy_poset(long id, char const *name_dst, char const *name_src);
 
-char const * npc_first_poset(long id);
-char const * npc_next_poset(long id, char const *name);
+char const *npc_first_poset(long id);
+char const *npc_next_poset(long id, char const *name);
 
 bool npc_add_relation(long id, char const *name, size_t x, size_t y);
 bool npc_is_relation(long id, char const *name, size_t x, size_t y);
@@ -26,7 +27,8 @@ size_t npc_poset_size();
 size_t npc_collection_size(long id);
 
 #ifdef __cplusplus
-  }
+} // extern "C"
+} // namespace cxx
 #endif
 
 #endif // header-guard
